625a65f03ff25fc82fad107ddbdaa554a313bccb
Abhinav Anand <abhinav.anand500@gmail.com>
2020-10-08 10:20:19 +0530

    XSS Attack

--- accrodin/src/component/Search.js
+++ accrodin/src/component/Search.js
@@ -1,8 +1,48 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function Search() {
     const [term, setTerm] = useState("");
-    console.log(term);
+    const [results, setResult] = useState([]);
+    console.log(results);
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get(
+                "https://en.wikipedia.org/w/api.php",
+                {
+                    params: {
+                        action: "query",
+                        list: "search",
+                        origin: "*",
+                        format: "json",
+                        srsearch: term,
+                    },
+                },
+            );
+            setResult(data.query.search);
+        };
+        if (term) {
+            search();
+        }
+    }, [term]);
+    const renderedResults = results.map((result, index) => {
+        return (
+            <div className='item' key={index}>
+                <div className='right floated content'>
+                    <a
+                        className='ui button'
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+                        Go
+                    </a>
+                </div>
+                <div className='content'>
+                    <div className='header'>{result.title}</div>
+                </div>
+                <span
+                    dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+            </div>
+        );
+    });
+
     return (
         <div className='ui form'>
             <div className='field'>
@@ -15,6 +55,7 @@ function Search() {
                     }}
                 />
             </div>
+            <div className='ui celled list'>{renderedResults}</div>
         </div>
     );
 }
