7a5538027222f3df8ee929e98f2bc2a88c56dde5
Abhinav Anand <abhinav.anand500@gmail.com>
2020-10-20 20:54:21 +0530

    SOng

---
+++ songs/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { selectSong } from "../actions";
+function App() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songReducer = () => {
+    return [
+        { title: "Pyar Kiya To Darna Kya", duration: "4.35" },
+        { title: "Honton Mein Aisi Baat Main", duration: "5.35" },
+        { title: "Khaike Paan Banaraswala", duration: "4.25" },
+        { title: "Yaara O Yaara Milna Hamara", duration: "3.35" },
+        { title: "Pinga", duration: "2.35" },
+        { title: "Chote Chote Bhaiyon Ke", duration: "4.59" },
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === "SONG_SELECTED") {
+        return action.payload;
+    }
+    return selectedSong;
+};
