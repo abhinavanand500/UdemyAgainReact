cd9d32b3d12f7824d9b462075cffeb6c26885b77
Abhinav Anand <abhinav.anand500@gmail.com>
2020-10-30 23:53:41 +0530

    Done

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,8 @@
 import jsonplaceholder from "../apis/jsonplaceholder";
 export const fetchPost = () => {
     return async (dispatch) => {
-        const response = await jsonplaceholder.get("/post");
+        const response = await jsonplaceholder.get("/posts");
+        console.log(response)
         dispatch({ type: "FETCH_POST", payload: response });
     };
 };
