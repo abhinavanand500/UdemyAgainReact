777b5d501d612efdc0f39a58fddeb0f9a72dd613
Abhinav Anand <abhinav.anand500@gmail.com>
2020-11-06 01:02:39 +0530

    React Router Set Up

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className='ui container'>
             <BrowserRouter>
                 <div>
-                    <Route path='/' component={App} />
+                    <Header />
+                    <Route path='/' exact component={StreamList} />
+                    <Route path='/streams/new' exact component={StreamCreate} />
+                    <Route path='/streams/edit' exact component={StreamEdit} />
+                    <Route
+                        path='/streams/delete'
+                        exact
+                        component={StreamDelete}
+                    />
+                    <Route path='/streams/show' exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
