97203f110515810661a586ecfc8a75723530e66f
Abhinav Anand <abhinav.anand500@gmail.com>
2020-11-13 02:46:52 +0530

    Redux-form problem sorted

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+class StreamCreate extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>StreamCreate</h1>
+            </div>
+        );
+    }
+}
 
-const StreamCreate = () => {
-    return (
-        <div>
-            <h1>StreamCreate</h1>
-        </div>
-    );
-};
-
-export default StreamCreate;
+export default reduxForm()(StreamCreate);
