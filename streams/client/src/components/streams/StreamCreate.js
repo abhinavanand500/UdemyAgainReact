c9c23c546f9532a8ba0a034d19152ef3a055603a
Abhinav Anand <abhinav.anand500@gmail.com>
2020-11-14 00:30:08 +0530

    Creating stream frontend

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,13 +1,52 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
-    render() {
+    renderInput({ input, label, meta }) {
+        console.log(meta);
         return (
-            <div>
-                <h1>StreamCreate</h1>
+            <div className='field'>
+                <label>{label}</label>
+                <input {...input} autoComplete='off' />
+                <div>{meta.error}</div>
             </div>
         );
     }
-}
 
-export default reduxForm()(StreamCreate);
+    onSubmit(formValues) {
+        console.log(formValues);
+    }
+    render() {
+        return (
+            <form
+                onSubmit={this.props.handleSubmit(this.onSubmit)}
+                className='ui form'>
+                <Field
+                    name='title'
+                    component={this.renderInput}
+                    label='Enter Title'
+                />
+                <Field
+                    name='description'
+                    component={this.renderInput}
+                    label='Enter Description'
+                />
+                <button className='ui button primary'>Submit</button>
+            </form>
+        );
+    }
+}
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = "You must enter any Title to proceed further";
+    }
+    if (!formValues.description) {
+        errors.description =
+            "You must enter any Description to proceed further";
+    }
+    return errors;
+};
+export default reduxForm({
+    form: "streamCreate",
+    validate,
+})(StreamCreate);
