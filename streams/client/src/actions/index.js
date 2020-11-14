22003c25ba0b44e291287bf9a9dedbc6976c868b
Abhinav Anand <abhinav.anand500@gmail.com>
2020-11-15 00:49:08 +0530

    HI

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,3 +1,5 @@
+import { formValues } from "redux-form";
+import streams from "../apis/streams";
 import { SIGN_IN, SIGN_OUT } from "./types.js";
 
 export const signIn = (userId) => {
@@ -12,3 +14,7 @@ export const signOut = () => {
         type: SIGN_OUT,
     };
 };
+
+export const createStreams = (formValues) =>asyc dispatch =>{   
+    streams.post('/streams',formValues)
+}
